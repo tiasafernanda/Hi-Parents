f3332042d16db14fcd10e57fed1584ded4cfab0a
tiasafernanda <tiasafernanda@gmail.com>
2021-11-29 13:54:56 +0700

    fixing pages

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,19 +1,19 @@
-import LoginNanny from "./auth/LoginNanny";
-import RegisterNanny from "./auth/RegisterNanny";
-import { useParams } from "react-router-dom";
-import React from "react";
+import LoginNanny from './auth/LoginNanny';
+import RegisterNanny from './auth/RegisterNanny';
+import { useParams } from 'react-router-dom';
+import React from 'react';
 
 export default function Auth() {
   const { type } = useParams();
-  console.log (type)
+  console.log(type);
   return (
     <div>
-      {type === "loginnanny" ? (
+      {type === 'signin' ? (
         <LoginNanny />
-      ) : type === "registernanny" ? (
+      ) : type === 'signup' ? (
         <RegisterNanny />
       ) : (
-        "Halaman tidak di temukan ini"
+        'Halaman tidak di temukan ini'
       )}
     </div>
   );
