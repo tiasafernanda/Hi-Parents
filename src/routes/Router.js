5f747eb69e2eb664f4d7080ded5abe472bda8643
tiasafernanda <tiasafernanda@gmail.com>
2021-11-29 13:47:21 +0700

    merging

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ClientDetail from '../pages/clientdetail/ClientDetail';
 import HomePage from '../pages/homepage/HomePage';
+import NannyDashboard from '../pages/nannydashboard/NannyDashboard';
 
 export default function Router() {
   return (
     <div>
       <Routes>
-        <Route path="/" exact element={<HomePage/>}/>
+        <Route path='/' exact element={<HomePage />} />
+        <Route path='/nanny' exact element={<NannyDashboard />} />
+        <Route path='/detail' exact element={<ClientDetail />} />
       </Routes>
     </div>
   );
