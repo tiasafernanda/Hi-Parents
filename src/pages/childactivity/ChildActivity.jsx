000c3797240a94f75f910041cd7141dd3c0f2776
tiasafernanda <tiasafernanda@gmail.com>
2021-11-29 17:30:06 +0700

    first commit

---
+++ src/pages/childactivity/ChildActivity.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './assets/ChildActivity.module.scss';
+
+export default function ChildActivity() {
+  return (
+    <div className={styles.container}>
+      <h1>this is child activity</h1>
+    </div>
+  );
+}
