f580162c8c1d32b0b41b24cc0593ef422808cc94
mdedyfauzi <mdedyfauzi@gmail.com>
2021-11-29 19:37:22 +0700

    fix:styling_layout

--- src/components/layout/DashboardIcons.jsx
+++ src/components/layout/DashboardIcons.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+/*import * as React from 'react';*/
 import SvgIcon from '@mui/material/SvgIcon';
 
 export function DashboardIcon(props) {
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/homepage/HomePage';
+import Layout from '../components/layout/Layout';
 
 export default function Router() {
   return (
     <div>
       <Routes>
-        <Route path="/" exact element={<HomePage/>}/>
+        <Route path="/" exact element={<Layout />} />
       </Routes>
     </div>
   );
