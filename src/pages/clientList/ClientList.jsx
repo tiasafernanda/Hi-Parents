6a43111957b305d3f04e60e7d18d36ace0adf0e6
mdedyfauzi <mdedyfauzi@gmail.com>
2021-12-01 22:07:04 +0700

    fix:client_list

--- src/pages/clientList/ClientList.jsx
+++ src/pages/clientList/ClientList.jsx
@@ -8,29 +8,33 @@ import { Link } from 'react-router-dom';
 import previousIcon from './assets/svg/previousicon.svg';
 import nextIcon from './assets/svg/nexticon.svg';
 
-function createData(dateRequest, parentName, clientId, childrenName) {
-  return { dateRequest, parentName, clientId, childrenName };
+function createData(dateRequest, parentName, clientId, childrenName, status) {
+  return { dateRequest, parentName, clientId, childrenName, status };
 }
 
 const clientDetail = [
-  createData('3/11/2021 07:00 PM', 'Yugi Muto', '#000123', 'Yugi Muto'),
-  createData('3/11/2021 07:00 PM', 'Nobitakun', '#000123', 'Nobitakun'),
-  createData('3/11/2021 07:00 PM', 'Nobitakun Desu Ne', '#000123', 'Nobitakun Desu Ne'),
-  createData('3/11/2021 07:00 PM', 'Nobitakun', '#000123', 'Nobitakun'),
+  createData('3/11/2021 07:00 PM', 'Yugi Muto', '#000120', 'Yugi Muto', 0),
+  createData('3/11/2021 07:00 PM', 'Nobitakun', '#000121', 'Nobitakun', 1),
+  createData('3/11/2021 07:00 PM', 'Nobitakun Desu Ne', '#000122', 'Nobitakun Desu Ne', 1),
+  createData('3/11/2021 07:00 PM', 'Doraemon to Nani', '#000123', 'Doraemon to Nani', 1),
+  createData('3/11/2021 07:00 PM', 'Sugoi Desu', '#000123', 'Sugoi Desu', 2),
 ];
 
 export default function ClientList() {
   const [open, setOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState('');
+  const modalValue = clientDetail.find((data) => data.clientId === selectedItem);
 
-  const handleClick = () => {
+  const handleClick = (clientId) => {
+    setSelectedItem(clientId);
     setOpen(true);
   };
 
-  const handleCLose = () => {
-    window.onclick = () => {
-      setOpen(false);
-    };
+  const handleCLose = (e) => {
+    setOpen(false);
   };
+
+  console.log(modalValue);
 
   return (
     <div className={styles.dashboard}>
@@ -46,37 +50,36 @@ export default function ClientList() {
             <th>Action</th>
           </tr>
           {clientDetail.map((item) => (
-            <tr>
+            <tr className={styles.row}>
               <td>{item.dateRequest}</td>
               <td>{item.parentName}</td>
               <td>{item.clientId}</td>
               <td>{item.childrenName}</td>
               <td>
                 <div className={styles.dropdown}>
-                  <button className={styles.statusButton}>Active</button>
-                  <div className={styles.dropdownContent}>
-                    <a className={styles.accept} onClick={() => handleClick()}>
-                      <span style={{ color: '#10B278', position: 'relative', top: '2px' }}>
-                        <BsCheck2Circle />
-                      </span>{' '}
-                      Accept Client
-                    </a>
-                    <a href="x">
-                      <span style={{ color: '#F67979', position: 'relative', top: '2px' }}>
-                        <BiXCircle />
-                      </span>{' '}
-                      Reject Client
-                    </a>
-                    <Link to="#">
-                      <span style={{ color: '#768471', position: 'relative', top: '2px' }}>
-                        <AiOutlineInfoCircle />
-                      </span>{' '}
-                      View Details
-                    </Link>
-                    <div onClick={() => handleCLose()} className={open === true ? styles.modalOnClick : styles.modal}>
-                      <Modal clientId={item.clientId} dateRequest={item.dateRequest} parentName={item.parentName} />
+                  <button className={item.status === 1 ? styles.statusActive : item.status === 2 ? styles.statusReject : styles.statusPending}>{item.status === 1 ? 'Active' : item.status === 2 ? 'Reject' : 'Pending'}</button>
+                  {item.status === 0 && (
+                    <div className={styles.dropdownContent}>
+                      <a className={styles.accept} onClick={() => handleClick(item.clientId)}>
+                        <span style={{ color: '#10B278', position: 'relative', top: '2px' }}>
+                          <BsCheck2Circle />
+                        </span>{' '}
+                        Accept Client
+                      </a>
+                      <a href="x">
+                        <span style={{ color: '#F67979', position: 'relative', top: '2px' }}>
+                          <BiXCircle />
+                        </span>{' '}
+                        Reject Client
+                      </a>
+                      <Link to="#">
+                        <span style={{ color: '#768471', position: 'relative', top: '2px' }}>
+                          <AiOutlineInfoCircle />
+                        </span>{' '}
+                        View Details
+                      </Link>
                     </div>
-                  </div>
+                  )}
                 </div>
               </td>
               <td>
@@ -84,42 +87,6 @@ export default function ClientList() {
               </td>
             </tr>
           ))}
-
-          {/*
-          <tr>
-            <td className={styles.noBorder}>3/11/2021 07:00 PM</td>
-            <td className={styles.noBorder}>Sugoi Desu</td>
-            <td className={styles.noBorder}>#000123</td>
-            <td className={styles.noBorder}>Sugoi Desu</td>
-            <td className={styles.noBorder}>
-              <div className={styles.dropdown}>
-                <button className={styles.statusButton}>Active</button>
-                <div className={styles.dropdownContent}>
-                  <a href="x">
-                    <span>
-                      <BsCheck2Circle style={{ color: '#10B278', position: 'relative', top: '2px' }} />
-                    </span>{' '}
-                    Accept Client
-                  </a>
-                  <a href="x">
-                    <span style={{ color: '#F67979', position: 'relative', top: '2px' }}>
-                      <BiXCircle />
-                    </span>{' '}
-                    Reject Client
-                  </a>
-                  <Link to="#">
-                    <span style={{ color: '#768471', position: 'relative', top: '2px' }}>
-                      <AiOutlineInfoCircle />
-                    </span>{' '}
-                    View Details
-                  </Link>
-                </div>
-              </div>
-            </td>
-            <td className={styles.noBorder}>
-              <button className={styles.actionButton}>&bull;&bull;&bull;</button>
-            </td>
-          </tr>*/}
         </table>
         <div className={styles.footer}>
           <p>Showing 10 of {clientDetail.length}</p>
@@ -134,6 +101,11 @@ export default function ClientList() {
             </button>
           </div>
         </div>
+        {open && (
+          <div onClick={(e) => handleCLose(e)}>
+            <Modal clientId={modalValue.clientId} dateRequest={modalValue.dateRequest} parentName={modalValue.parentName} />
+          </div>
+        )}
       </div>
     </div>
   );
